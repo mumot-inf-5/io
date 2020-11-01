b92b5d01314f3b009aee425a96d5ffded54a91f4
Bartłomiej Mumot <mumot1999@gmail.com>
2020-11-01 21:28:33 +0100

    Zmiany w commitach

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,9 @@
+//Dodaje losowy komentarz
 public class Program {
   //pierwsza zmiana
     public static void main(String[] args){
       //druga zmiana
       System.out.print("Hello World");
-      //trzecia zmiana
+      //trzecia zmiana 3
     }
   }
