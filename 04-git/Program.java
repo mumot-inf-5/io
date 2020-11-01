3d740ca6ac7beafe9e2afe785916384d07ff2467
Bartłomiej Mumot <mumot1999@gmail.com>
2020-11-01 17:27:39 +0100

    first new change

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,5 +1,8 @@
 public class Program {
+  //pierwsza zmiana
     public static void main(String[] args){
+      //druga zmiana
       System.out.print("Hello World");
+      //trzecia zmiana
     }
   }
